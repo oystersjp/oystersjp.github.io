7e3f0e05d9dde645555ad82f044d58ceb1e9c754
YuMuuu <YuMuuu@users.noreply.github.com>
2021-02-15 16:57:00 +0000

    deploy: 491b9326f8b75254fbf7c7977a12cce132391b79

--- _next/static/93xwrzDkPNt3XonMkBc3f/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-58cdddf28f10f88cfdfb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-90c269e30d7d67345417.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/uRSFIaYij_dEUnjjJQgzt/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-86952bbb5eabba39be5c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3fe5d11eaace076cd444.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
