b39a5ca4a134bdaac97fb54279d4eb9de8661891
mii288 <atteeen@gmail.com>
2021-02-24 00:10:25 +0900

    chore: remove unnecessary comments

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,5 +1,3 @@
-// NOTE: airbnb/babel-plugin-inline-react-svgがaliasに非対応のため相対パスで読み込んでいます
-// https://github.com/airbnb/babel-plugin-inline-react-svg/pull/17
 import LogoSVG from '@/components/Logo/logo.svg'
 import { Svg } from 'react-optimized-image'
 
