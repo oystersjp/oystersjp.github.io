43e5d2e7358999df0aa71dc2d2941f334ef68acb
mii288 <atteeen@gmail.com>
2021-02-23 23:23:24 +0900

    chore: support for module path aliases #103

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    return config
+  }
+}
+
+module.exports = nextConfig
