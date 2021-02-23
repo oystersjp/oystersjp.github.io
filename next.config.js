5c93d33d9599e6ac5e6244d58076ea5c94a883de
mii288 <atteeen@gmail.com>
2021-02-23 23:25:06 +0900

    chore: use next-optimized-images instead of babel-plugin-inline-react-svg

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 const path = require('path')
+const withPlugins = require('next-compose-plugins')
+const optimizedImages = require('next-optimized-images')
+
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname)
     return config
   }
 }
-
-module.exports = nextConfig
+module.exports = withPlugins(
+  /**
+   * next-optimized-images
+   * https://github.com/cyrilwanner/next-optimized-images#configuration
+   */
+  [optimizedImages],
+  nextConfig
+)
