3e84d94d2c51b769c18008d84f5beb03c6de0f0b
mii288 <atteeen@gmail.com>
2021-04-11 02:42:30 +0900

    fix: readonlyにした

--- components/About/constants.ts
+++ components/About/constants.ts
@@ -1,4 +1,4 @@
-export const SLIDES = ['./images/slide/1.jpg', './images/slide/2.jpg']
+export const SLIDES = ['./images/slide/1.jpg', './images/slide/2.jpg'] as const
 export const ABOUT_ITEMS = [
   {
     title: 'We Are Junior Creators',
@@ -16,4 +16,4 @@ export const ABOUT_ITEMS = [
       '(主に) web系のトピックについて話したり、一緒になんか作ったり、モクモクしたりしています',
     icon: 'tf-ion-beer'
   }
-]
+] as const
