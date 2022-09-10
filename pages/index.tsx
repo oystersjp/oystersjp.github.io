76faea5d40993c3066103d27bd8a0ee58862a8df
mii288 <mii288@users.noreply.github.com>
2022-09-11 06:25:15 +0900

    fix: 外部CSSの読み込みを _document.tsx で実施
    Next12から_document.tsxで読み込むことが推奨になったので対応

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,12 +39,6 @@ export default function Home({ members }: { members: Member[] }): JSX.Element {
       <Head>
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossOrigin="anonymous"
-        />
 
         {metaList.map((item) => (
           <meta
