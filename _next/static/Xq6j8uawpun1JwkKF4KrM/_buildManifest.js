ef03a040fdf7dc80317a563d88172124ca5c0cd7
mii288 <mii288@users.noreply.github.com>
2022-09-11 05:22:22 +0000

    deploy: b1b07acb6dad7ceda78faf17775931405cbb15e2

--- _next/static/Xq6j8uawpun1JwkKF4KrM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-daeeb1bbb2469f15873d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d7e62032df1364e097de.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/lGmcz0gh8zLvXi5Z3UI-X/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/682-e297cedb63b87e0c.js","static/chunks/pages/index-f708e80c554adbb8.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
