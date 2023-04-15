74ad382c073ab1cf6624d64ea08a2d2984365ccb
Kazuki Otsu <cocoeyes02@gmail.com>
2023-04-16 02:22:20 +0900

    refactor(GA): React Fragmentで二重に囲わなくて良いところは冗長なので削除

--- lib/googleTagManager.tsx
+++ lib/googleTagManager.tsx
@@ -5,20 +5,18 @@ const googleTagManagerId = 'GTM-KH27S7S'
 export const GoogleTagManager = (): JSX.Element => (
   <>
     {
-      <>
-        <Script
-          id="gtm-init"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${googleTagManagerId}');
-            `
-          }}
-          strategy="afterInteractive"
-        />
-      </>
+      <Script
+        id="gtm-init"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${googleTagManagerId}');
+          `
+        }}
+        strategy="afterInteractive"
+      />
     }
   </>
 )
@@ -26,19 +24,17 @@ export const GoogleTagManager = (): JSX.Element => (
 export const NoScript = (): JSX.Element => (
   <>
     {
-      <>
-        <noscript
-          id="ns-init"
-          dangerouslySetInnerHTML={{
-            __html: `<iframe
-              src="https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            />`
-          }}
-        />
-      </>
+      <noscript
+        id="ns-init"
+        dangerouslySetInnerHTML={{
+          __html: `<iframe
+            src="https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          />`
+        }}
+      />
     }
   </>
 )
