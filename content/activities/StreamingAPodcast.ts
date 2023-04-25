740b0b213f53c5c7d1b5b5c94ed6e141b5ea3727
Kazuki Otsu <cocoeyes02@gmail.com>
2023-04-25 21:34:14 +0900

    feat(Activity): 活動内容にPodcastサイトを追加した

---
+++ content/activities/StreamingAPodcast.ts
@@ -0,0 +1,13 @@
+import Activitiy from '@/types/Activity'
+
+const StreamingAPodcast: Activitiy = {
+  title: 'Streaming A Podcast',
+  imagePath: 'images/podcast.jpg',
+  alt: 'Podcast サイトの一覧画面です',
+  content:
+    'Podcastで所属メンバーが技術のことから暮らしのことまで語っています。',
+  linkTitle: 'Podcast サイト',
+  url: 'https://oystersjp.github.io/podcast/'
+}
+
+export default StreamingAPodcast
