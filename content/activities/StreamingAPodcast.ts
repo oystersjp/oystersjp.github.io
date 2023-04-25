22f69fa7f03d73ccc1178b6504d2fdda7bcd68f5
02 <cocoeyes02@gmail.com>
2023-04-25 23:47:32 +0900

    Update content/activities/StreamingAPodcast.ts
    Co-authored-by: mii288 <3056652+mii288@users.noreply.github.com>

--- content/activities/StreamingAPodcast.ts
+++ content/activities/StreamingAPodcast.ts
@@ -5,7 +5,7 @@ const StreamingAPodcast: Activitiy = {
   imagePath: 'images/podcast.jpg',
   alt: '',
   content: 'Podcastで所属メンバーが技術のことから暮らしのことまで語っています',
-  linkTitle: 'Podcast サイト',
+  linkTitle: 'Oysters Podcast',
   url: 'https://oystersjp.github.io/podcast/'
 }
 
