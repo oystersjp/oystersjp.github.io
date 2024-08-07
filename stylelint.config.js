875e42f19d802fddf499a5c738e0e6de579c7637
mii288 <mii288@users.noreply.github.com>
2024-08-08 00:04:13 +0900

    chore: Stylelintセットアップ

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-html/html',
+    'stylelint-config-html/xml',
+    'stylelint-config-html/astro'
+  ],
+  rules: {}
+}
