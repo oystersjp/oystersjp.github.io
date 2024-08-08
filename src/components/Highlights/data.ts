3f86e02e0cf49429041dabc4ca70312624e3ec1a
mii288 <mii288@users.noreply.github.com>
2024-08-08 22:28:35 +0900

    feat: 作ったもの、実績追加

---
+++ src/components/Highlights/data.ts
@@ -0,0 +1,43 @@
+export const highlights = [
+  {
+    image: 'isucon.png',
+    description: '毎年isuconに参加。2018年度は本選に出場',
+    link: {
+      title: 'isuconレポート',
+      url: 'https://blog.pinkumohikan.com/entry/isucon13'
+    }
+  },
+  {
+    image: 'mukimuki.jpg',
+    description: '毎年1・4・7・10月に振り返り会を実施',
+    link: {
+      title: 'pinkumohikan/ムキムキ会 振り返りテンプレ.md',
+      url: 'https://gist.github.com/pinkumohikan/759f2bcde44a39fbeb94d59ebc34c3d5'
+    }
+  },
+  {
+    image: 'gohan-dekitayo.png',
+    description: 'Discordのボイスチャンネルに入室したことをSlackに通知するBot',
+    link: {
+      title: 'oystersjp/gohan-dekitayo',
+      url: 'https://github.com/oystersjp/gohan-dekitayo'
+    }
+  },
+  {
+    image: 'mugen-lt.png',
+    description:
+      'その場でスライドを作り即発表するがコンセプトの登壇イベント「無限LT」を企画・運営',
+    link: {
+      title: 'connpassイベントページ',
+      url: 'https://oysters.connpass.com'
+    }
+  },
+  {
+    image: 'coin-step.png',
+    description: '仮想通貨のヴァーチャルトレードサービス「Coin Step」'
+  },
+  {
+    image: 'penguin-news.png',
+    description: '2ちゃんねるまとめサイト「ペンギンニュース」'
+  }
+] as const
