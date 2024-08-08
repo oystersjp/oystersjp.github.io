822ad6f5f15f7082834a09e144cd1e4567ad5697
mii288 <mii288@users.noreply.github.com>
2024-08-08 17:35:39 +0900

    feat: tailwindのセットアップ

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 /** @type {import('lint-staged').Config} */
 export default {
   '*.{css,scss,astro}': ['npm run fix:css', 'npm run fix:format'],
-  '*.{js,cjs,ts,jsx,tsx,astro}': [
+  '*.{js,cjs,mjs,ts,jsx,tsx,astro}': [
     'npm run fix:js',
     'npm run fix:format',
     () => 'npm run type-check'
