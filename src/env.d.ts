937efe30959f12318bbbe191685045a384d55173
mii288 <mii288@users.noreply.github.com>
2024-11-20 15:49:34 +0900

    perf: 本番ビルド時にdebug用のモジュールを含めないように修正

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  /** Hero部分の背景エフェクトにデバッグ情報を表示するか否か */
+  readonly PUBLIC_HERO_EFFECT_ENABLE_DEBUG: boolean
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
