53af93101ff18123b2d3b771a52db7451022eba7
mii288 <mii288@users.noreply.github.com>
2024-12-05 15:42:21 +0900

    feat(vrt): baseブランチの差分と比較できるように修正

--- tests/vrt.spec.ts
+++ tests/vrt.spec.ts
@@ -2,19 +2,14 @@ import { test } from '@playwright/test'
 
 test.describe('Capture screenshots', () => {
   const workDir = '.reg'
-  const actualDir = `${workDir}/actual`
-  const expectedDir = `${workDir}/expected`
+  const saveDir = `${workDir}/screenshots`
 
   test('Home', async ({ page }, info) => {
-    for (const dir of [expectedDir, actualDir]) {
-      const isExpectedDir = expectedDir === dir
-
-      await page.goto(isExpectedDir ? 'https://oysters.dev' : '/')
-      await page.screenshot({
-        path: `${dir}/${info.title}/${info.project.name}.png`,
-        fullPage: true,
-        animations: 'disabled'
-      })
-    }
+    await page.goto('/')
+    await page.screenshot({
+      path: `${saveDir}/${info.title}/${info.project.name}.png`,
+      fullPage: true,
+      animations: 'disabled'
+    })
   })
 })
