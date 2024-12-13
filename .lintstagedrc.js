cb67abfbc674ba68033450f3406380c35296b8ea
mii288 <mii288@users.noreply.github.com>
2024-12-13 12:45:35 +0900

    fix: lint-stagedで*.spec.tsを対象に含めるように修正

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,10 @@
 /** @type {import('lint-staged').Config} */
 export default {
-  '*.{css,scss,astro}': ['npm run fix:css', 'npm run fix:format'],
-  '*.{js,cjs,mjs,ts,jsx,tsx,astro}': [
+  '**.{css,scss,astro}': ['npm run fix:css', 'npm run fix:format'],
+  '**.{js,cjs,mjs,ts,jsx,tsx,astro}': [
     'npm run fix:js',
     'npm run fix:format',
     () => 'npm run type-check'
   ],
-  '*.{md,html,json,yaml,yml}': ['npm run fix:format']
+  '**.{md,html,json,yaml,yml}': ['npm run fix:format']
 }
