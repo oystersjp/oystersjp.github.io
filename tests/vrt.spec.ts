935698d0b1bd36040edcdc4865c4fcf6e05558d0
mii288 <mii288@users.noreply.github.com>
2024-12-13 17:43:31 +0900

    fix(vrt): スクリーンショット時に一部画像が読み込まれていない問題の修正

--- tests/vrt.spec.ts
+++ tests/vrt.spec.ts
@@ -5,6 +5,18 @@ test.describe('Capture screenshots', () => {
 
   test('Home', async ({ page }, info) => {
     await page.goto('/')
+
+    // loading='lazy'が指定されている画像を読み込む
+    await page.evaluate(async () => {
+      const browserHeight = window.innerHeight
+      const pageHeight = document.body.scrollHeight
+
+      for (let i = 0; i < pageHeight; i += browserHeight) {
+        window.scrollTo(0, i)
+        await new Promise((r) => window.setTimeout(r, 100))
+      }
+    })
+
     await page.screenshot({
       path: `${saveDir}/${info.title}/${info.project.name}.png`,
       fullPage: true,
