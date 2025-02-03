eb18db660dc27a9f1554b0bef05eb6c781692e6a
mii288 <mii288@users.noreply.github.com>
2025-02-03 08:23:33 +0000

    deploy: 3e11525a6cde0f875b82b636e5ff6c71b0e33256

--- _next/static/-jPRi0voXBTq_GDB_Pksy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/682-e297cedb63b87e0c.js","static/chunks/pages/index-b93bb0ac03bdfa6d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
