a70ae474e4f4af2e3b8081387cb7ab7ba06d5269
mii288 <mii288@users.noreply.github.com>
2025-06-15 23:24:38 +0900

    feat(vrt): baseブランチの差分と比較できるようにした

--- tests/vrt.spec.ts
+++ tests/vrt.spec.ts
@@ -1,33 +1,28 @@
 import { expect, test } from '@playwright/test'
 test.describe('Capture screenshots', () => {
   const workDir = '.reg'
-  const actualDir = `${workDir}/actual`
-  const expectedDir = `${workDir}/expected`
+  const saveDir = `${workDir}/screenshots`
 
   test('Home', async ({ page }, info) => {
     // 背景エフェクトのアニメーションを無効化
     await page.emulateMedia({ reducedMotion: 'reduce' })
 
-    for (const dir of [expectedDir, actualDir]) {
-      const isExpectedDir = expectedDir === dir
-      await page.goto(isExpectedDir ? 'https://oysters.dev' : '/')
+    await page.goto('/')
+    // loading='lazy'が指定されている画像を読み込む
+    await page.evaluate(async () => {
+      const browserHeight = window.innerHeight
+      const pageHeight = document.body.scrollHeight
 
-      // loading='lazy'が指定されている画像を読み込む
-      await page.evaluate(async () => {
-        const browserHeight = window.innerHeight
-        const pageHeight = document.body.scrollHeight
+      for (let i = 0; i < pageHeight; i += browserHeight) {
+        window.scrollTo(0, i)
+        await new Promise((r) => window.setTimeout(r, 100))
+      }
+    })
 
-        for (let i = 0; i < pageHeight; i += browserHeight) {
-          window.scrollTo(0, i)
-          await new Promise((r) => window.setTimeout(r, 100))
-        }
-      })
-
-      await page.screenshot({
-        path: `${dir}/${info.title}/${info.project.name}.png`,
-        fullPage: true,
-        animations: 'disabled'
-      })
-    }
+    await page.screenshot({
+      path: `${saveDir}/${info.title}/${info.project.name}.png`,
+      fullPage: true,
+      animations: 'disabled'
+    })
   })
 })
